28dff18a278ce56cb5e4b59dbfe455d4c32e831c
ManuDev03 <manuprajan03@gmail.com>
2021-08-05 17:51:51 +0530

    Observable sevices Subjects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { UserService } from './user.service';
+import { Subscription } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit,OnDestroy {
   title = 'observable';
+  userActivated = false;
+  private activatedSub: Subscription;
+
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit() {
+    this.activatedSub = this.userService.activatedEmitter.subscribe(didActivate => {
+      this.userActivated = didActivate;
+    });
+  }
+   ngOnDestroy(): void {
+    this.activatedSub.unsubscribe();
+  }
+
 }
